fb68a2e2f91a467518bfe16194ab0e4c1e441779
Diptee Parmar <dreamtechnolab@Diptees-iMac.local>
2021-10-05 11:43:51 +0530

    make it better

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const pool = mysql.createPool({
-host: "localhost",
-user: "root",
-//   password: "password",
-database: "just-order",
+host: "sql6.freemysqlhosting.net",
+user: "sql6441797",
+password: "9m2SNrVi8F",
+database: "sql6441797",
 });
 
 module.exports = pool;
